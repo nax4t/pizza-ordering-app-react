c78f0616bd32bbd9c9cc85a419e050bbb2c97232
Mohsin Pannu <mohsin.pannu170@gmail.com>
2023-12-03 22:41:49 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './ui/Home'
+import Menu, { loader as MenuLoader } from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import CreateOrder from './features/order/CreateOrder'
+import Order, { loader as OrderLoader } from './features/order/Order'
+import AppLayout from './ui/AppLayout'
+import Error from './ui/Error'
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: MenuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: OrderLoader,
+        errorElement: <Error />,
+      },
+    ],
+  },
+])
+
+const App = () => {
+  return <RouterProvider router={router} />
+}
+
+export default App
